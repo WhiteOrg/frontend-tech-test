61cf6b488229497d85a7d1fb7020cc16be4a5766
theAdhocracy <murrayadcock@gmail.com>
2020-08-06 12:54:25 +0100

    Personalising initial screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./stech.svg";
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="Stech logo." />
+			<h1>Stech Frontend Challenge</h1>
+			<p>
+				Edit <code>src/App.js</code> and save to reload.
+			</p>
+			<a
+				className="App-link"
+				href="https://github.com/WhiteOrg/frontend-tech-test"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Full Instructions
+			</a>
+		</header>
 	);
 }
 
