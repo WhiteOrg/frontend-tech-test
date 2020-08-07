0f5af69d3f66dd573a212bf2aba7a65712bee721
BrunoWilkinson <bruno.wilkinson@gmail.com>
2020-08-07 16:55:58 +0100

    feat: add bullet

---
+++ src/components/atoms/bullet/index.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Bullet = styled.li`
+  font-size: 14px;
+  line-height: 22px;
+`;
+
+export default Bullet;
---
+++ src/components/atoms/stories/Bullet.stories.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Bullet from "../bullet";
+
+export default {
+  title: "Atoms",
+};
+
+export const bullet = () => (
+  <ul>
+    <Bullet>
+      Advertising is key factor in your web site promotion. Let people talk
+      about you, your products, and your services.
+    </Bullet>
+    <Bullet>
+      You see, people are always on the look out for good content right? Your
+      site should have really good contents. Don’t write
+    </Bullet>
+    <Bullet>
+      “Welcome to my website” or “Ours is a World’s first company…blah blah”
+      unless and until you really have dominating product.{" "}
+    </Bullet>
+    <Bullet>
+      You sell your products or services as like others, but you think you must
+      be noticed.{" "}
+    </Bullet>
+    <Bullet>
+      Your offer should be different to attract visitor. Your presentation
+      should be mind catching.{" "}
+    </Bullet>
+    <Bullet>
+      Get aware of advertisers online. There are few major players around.{" "}
+    </Bullet>
+  </ul>
+);
