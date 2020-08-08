592ab72869543a4a672a83791fedae91d1bb0187
BrunoWilkinson <bruno.wilkinson@gmail.com>
2020-08-08 16:24:45 +0100

    feat: add list

--- src/components/molecules/list/index.tsx
+++ src/components/molecules/list/index.tsx
@@ -1,10 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+import Paragraph from "../../atoms/paragraph";
 
-const Container = styled.div``;
+type Prize = {
+  title: string;
+  total: string;
+};
 
-const List: React.FunctionComponent = () => {
-  return <Container></Container>;
+type Props = {
+  prizeList: Prize[];
+};
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  max-height: 300px;
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f5f4fe;
+    border-radius: 5px;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #f9cb11;
+    border-radius: 5px;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #d8b00f;
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+  border-bottom: 1px solid #f5f4fe;
+  justify-content: space-between;
+  & > p {
+    flex: 1;
+  }
+  & > .title-col {
+    flex: 10;
+  }
+`;
+
+const List: React.FunctionComponent<Props> = ({ prizeList }) => {
+  return (
+    <Container>
+      {prizeList.map((prize, index) => (
+        <Row>
+          <Paragraph size="medium" textColor="black" bold>
+            {index + 1}
+          </Paragraph>
+          <Paragraph className="title-col" size="medium" textColor="black" bold>
+            {prize.title}
+          </Paragraph>
+          <Paragraph size="medium" textColor="black" bold>
+            {prize.total}
+          </Paragraph>
+        </Row>
+      ))}
+    </Container>
+  );
 };
 
 export default List;
--- src/components/molecules/stories/List.stories.js
+++ src/components/molecules/stories/List.stories.js
@@ -5,8 +5,51 @@ export default {
   title: "Molecules",
 };
 
+const prizeList = [
+  {
+    title: "P.C",
+    total: "£2,500.00",
+  },
+  {
+    title: "J.S",
+    total: "£1,250.00",
+  },
+  {
+    title: "H.J",
+    total: "£700.00",
+  },
+  {
+    title: "J.V",
+    total: "£300.00",
+  },
+  {
+    title: "L.K",
+    total: "£100.00",
+  },
+  {
+    title: "H.D",
+    total: "£50.00",
+  },
+  {
+    title: "V.G",
+    total: "£35.00",
+  },
+  {
+    title: "D.C",
+    total: "£20.00",
+  },
+  {
+    title: "P.D",
+    total: "£10.00",
+  },
+  {
+    title: "L.M",
+    total: "£5.00",
+  },
+];
+
 export const list = () => (
   <div>
-    <List />
+    <List prizeList={prizeList} />
   </div>
 );
