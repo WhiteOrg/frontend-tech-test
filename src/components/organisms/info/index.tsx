1c1192da2a2e7a6ed2f3cc6c89d109bb6a1ee304
BrunoWilkinson <bruno.wilkinson@gmail.com>
2020-08-08 17:49:56 +0100

    feat: setup info section

---
+++ src/components/organisms/info/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import CountDown from "../../molecules/countdown";
+import PoolInfo from "../../molecules/pool_info";
+import StartDate from "../../molecules/start_date";
+import { TitleH1 } from "../../atoms/title";
+import Button from "../../atoms/button";
+import Icon from "../../atoms/icon";
+import close from "../../../images/close.svg";
+import user from "../../../images/user.svg";
+import calendar from "../../../images/calendar.svg";
+
+type Props = {
+  readonly title: string;
+  readonly prizePool: string;
+  readonly players: number;
+  readonly startDate: string;
+  readonly endDate: string;
+  readonly backgroundImage: string;
+};
+
+const Container = styled.section``;
+
+const TitleRow = styled.div``;
+
+const InfoRow = styled.div``;
+
+const Info: React.FunctionComponent<Props> = (props) => (
+  <Container>
+    <TitleRow>
+      <TitleH1 textColor="white">{props.title}</TitleH1>
+      <Icon src={close} rotate={false} />
+    </TitleRow>
+    <CountDown endDate={props.endDate} />
+    <InfoRow>
+      <PoolInfo title="prizePool" info={props.prizePool} />
+      <PoolInfo title="player" info={props.players.toString()} icon={user} />
+    </InfoRow>
+    <StartDate
+      info="Starts on Thu, 20 Feb 2020, 23:00:00 GMT"
+      icon={calendar}
+    />
+    <Button fullWidth />
+  </Container>
+);
+
+export default Info;
---
+++ src/components/organisms/stories/Info.stories.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Info from "../info";
+
+export default {
+  title: "Organisms",
+};
+
+const data = {
+  title: "Play'n GO Tournament",
+  endDate: "2020-15-06T10:06:29.578Z",
+  startDate: "2020-08-06T10:06:29.578Z",
+  prizePool: "£25,000.00",
+  backgroundImage: "https://i.imgur.com/W4WBNV2.jpg",
+  players: 250,
+};
+
+export const info = () => (
+  <Info
+    title={data.title}
+    prizePool={data.prizePool}
+    players={data.players}
+    startDate={data.startDate}
+    endDate={data.endDate}
+    backgroundImage={data.backgroundImage}
+  />
+);
