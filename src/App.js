b2686d78132869754af798e08d5886eb0ff2992d
Jaspaul Aggarwal <Jaspaul.Aggarwal@dixonscarphone.com>
2020-08-23 09:47:26 +0100

    initial start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
-import logo from "./images/stech.svg";
 import "./App.css";
 
 function App() {
 	return (
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="Stech logo." />
-			<h1>Stech Frontend Challenge</h1>
-			<p>
-				Edit <code>src/App.js</code> and save to reload.
-			</p>
-			<a
-				className="App-link"
-				href="https://github.com/WhiteOrg/frontend-tech-test"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Full Instructions
-			</a>
-		</header>
+		<p>Initial App</p>
 	);
 }
 
